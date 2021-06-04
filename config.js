06581bf07adf6e4b2e34e919ba506f876105dbbd
Armand Bautista <agb7582@yahoo.com>
2021-06-03 18:15:25 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  'facebook': {
+    clientId: '303197024606696',
+    clientSecret: 'cf4b84dd583495bb0c582e4dbc88d855'
+}
 };
